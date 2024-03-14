893e9b383ecdb56e857d31e8e0ca05d7943b1130
Jorge Flores <720624@student.dusd.net>
2024-03-14 11:43:07 -0700

    I have added new screipts

--- P3UNIT2Flores/Assets/Scripts/PlayerControler.cs
+++ P3UNIT2Flores/Assets/Scripts/PlayerControler.cs
@@ -7,7 +7,7 @@ public class PlayerControler : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
-    
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +17,11 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
         // Keep the player in bounds
         if(transform.position.x < -xRange)
         {
@@ -26,7 +31,7 @@ public class PlayerControler : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        horizontalInput = Input.GetAxis("Hoorizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
     }
