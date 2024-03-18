838ae80cb7ecd27bcb358d526d3f4d28a69e964f
Jorge Flores <720624@student.dusd.net>
2024-03-18 11:43:08 -0700

    finshedd to 2.3

--- P3UNIT2Flores/Assets/Scripts/PlayerControler.cs
+++ P3UNIT2Flores/Assets/Scripts/PlayerControler.cs
@@ -17,9 +17,8 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+       if (Input.GetKeyDown(KeyCode.Space))
         {
-            //Launch a projectile from the player
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
         // Keep the player in bounds
